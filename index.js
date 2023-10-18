266a5f910e3335350e00bcffdde63305d4b0db19
alexandermcmillan <mcmillancontact@protonmail.com>
2023-10-18 09:53:16 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,30 +1,115 @@
-const employee = {};
+
+const employee = {
+  name: "Bob",
+  streetAddress: "111 1st street",
+};
 
 
 
+// //function nondestructivelyUpdateObject(obj, key, value) {
+//   const newObj = { ...obj };
+
+//   newObj[key] = 
+//   value;
+
+//   return newObj;
+// }
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+
+  newEmployee[key] = value;
+
+  return newEmployee;
+};
 
 
 
-function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
-  return Object.assign({}, employee, {[name]: streetAddress})
-}
+// destructivelyUpdateEmployeeWithKeyAndValue(): this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee Object passed in.
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
   employee[key] = value;
+
   return employee;
-}
+
+};
+
+// deleteFromEmployeeByKey(): this function should take in a employee Object and a key. It should delete the property with that key from the employee Object. This should not mutate the original employee Object; it should return a new Object that doesn't include the identified key-value pair. Hint: use the spread operator!
 
 
+function deleteFromEmployeeByKey(employee, name) {
+  const newerEmployee = { ...employee, }
 
-function deleteFromEmployeeByKey(employee, key) {
-  const copyOfEmployee = {...employee}
-  delete copyOfEmployee [key]
-  return copyOfEmployee
+  delete newerEmployee.name
 
+  return newerEmployee
+};
 
-}
+// destructivelyDeleteFromEmployeeByKey(): this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object.
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-  delete employee [key]
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+  delete employee.name
+
   return employee
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {};
+
+
+
+
+
+
+// function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+//   return Object.assign({}, employee, {[name]: streetAddress})
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//   employee[key] = value;
+//   return employee;
+// }
+
+
+
+// function deleteFromEmployeeByKey(employee, key) {
+//   const copyOfEmployee = {...employee}
+//   delete copyOfEmployee [key]
+//   return copyOfEmployee
+
+
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//   delete employee [key]
+//   return employee
+// }
